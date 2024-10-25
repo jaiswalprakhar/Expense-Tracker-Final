0e85e568fbfa1df946d66ba2d3b250fbdb6d8d49
Prakhar Jaiswal <jaiswalprakhar98@gmail.com>
2024-10-25 20:41:37 +0530

    BACKEND

--- FRONTEND/components/JS/loadContent.js
+++
@@ -1,9 +0,0 @@
-export const loadContent = () => {
-  axios.get('../Layout/header.html')
-    .then(response => {
-      document.querySelector('header').innerHTML = response.data;
-    })
-    .catch(error => {
-      console.error('Error loading header:', error);
-    });
-}
